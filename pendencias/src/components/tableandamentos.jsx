af1889fa8bd4bac41c627262842e6708e288e436
LUIZ EDUARDO KROL PEREIRA <luiz.pereira@redeunifique.com.br>
2023-12-30 03:09:58 -0300

    fix: consertado tablehover muito grande saindo da tela

--- pendencias/src/components/tableandamentos.jsx
+++ pendencias/src/components/tableandamentos.jsx
@@ -1,8 +1,12 @@
 import moment from 'moment-timezone';
-
+import { useEffect, useRef, useState } from 'react';
 
 function TableAndamentos( { pendencias, id, top, handleMouseOut, handleMouseOver } ) {
     const pendenciaId = pendencias.find((penden) => penden.id == id)
+    const bott = window.innerHeight;
+    const divRef = useRef(null);
+    const [maxH, setMaxH] = useState(0);
+
 
     function formataData(date) {
         date = moment(date);
@@ -11,8 +15,23 @@ function TableAndamentos( { pendencias, id, top, handleMouseOut, handleMouseOver
     } 
 
 
+    useEffect(() => {   //caso a div for sair da tela seta o maxH pra não sair
+        const divee = divRef.current.getBoundingClientRect().bottom; 
+        if (divee > bott) setMaxH(divee - bott);
+    }, []) 
+
     return (
-        <div onMouseLeave={handleMouseOut} onMouseOver={handleMouseOver} className="mt-2 border-4 border-[#212121] absolute right-[1%]" style={{top: top}} >
+        <div 
+            ref={divRef}
+            onMouseLeave={handleMouseOut} 
+            onMouseOver={handleMouseOver} 
+            className="mt-2 border-4 border-[#212121] absolute right-[1%] overflow-auto min-w-[520px] max-w-[1200px]" 
+            style={{
+                top: top,
+                maxHeight: maxH > 0 ? `${maxH}px` : 'none', 
+                overflowY: maxH > 0 ? 'visible' : 'none'
+            }} 
+        >
             <table className="w-full font-Inter text-xs font-light rounded text-[#ffffffde]">
                 <thead className="border-b-2 border-[#292929] font-Inter font-thin">
                     <tr>
