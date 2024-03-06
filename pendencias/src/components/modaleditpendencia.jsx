e24dd53bf7115bdc1599d816e20f0957d9eac901
Luiz Eduardo <luizeduardokrolpereira@gmail.com>
2024-03-06 19:01:23 -0300

    feat: switch button unidades

--- pendencias/src/components/modaleditpendencia.jsx
+++ pendencias/src/components/modaleditpendencia.jsx
@@ -3,6 +3,7 @@ import { AiOutlineClose } from "react-icons/ai";
 import useAxiosPrivate from "../Hooks/useAxiosPrivate";
 import moment from "moment-timezone";
 
+//TODO: botão alternar unidade
 function ModalEditPendencia({ fecharModal, penden }) {
   const [formData, setFormData] = useState({
     titulo: penden.titulo,
@@ -21,10 +22,12 @@ function ModalEditPendencia({ fecharModal, penden }) {
       user: penden.fechamento.user,
     },
     massiva: penden.massiva,
+    unidade: penden.unidade,
   });
   const [tipos, setTipos] = useState();
   const axiosPrivate = useAxiosPrivate();
   const agr = Date.now();
+  const [isChecked, setIsChecked] = useState(formData.unidade === "SYGO");
 
   useEffect(() => {
     const controller = new AbortController();
@@ -61,6 +64,10 @@ function ModalEditPendencia({ fecharModal, penden }) {
     };
   }, [fecharModal]);
 
+  useEffect(() => {
+    setIsChecked(formData.unidade === "SYGO");
+  }, [formData.unidade]);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
@@ -83,6 +90,14 @@ function ModalEditPendencia({ fecharModal, penden }) {
     const { name, value, type, checked } = e.target;
     const newValue = type === "checkbox" ? checked : value;
     setFormData({ ...formData, [name]: newValue });
+  };
+
+  const handleUniChange = () => {
+    if (formData.unidade === "SYGO") {
+      setFormData({ ...formData, unidade: "TIO" });
+    } else {
+      setFormData({ ...formData, unidade: "SYGO" });
+    }
   };
 
   function formataData(date) {
@@ -253,31 +268,55 @@ function ModalEditPendencia({ fecharModal, penden }) {
                   </label>
                 </div>
               </div>
-              <div className="flex w-90% py-2 border-b-2 border-[#efefef] pb-3">
-                <input
-                  value={formData.massiva}
-                  onChange={handleInputChange}
-                  type="checkbox"
-                  checked={formData.massiva}
-                  className="peer mt-1 mr-2 w-4 h-4 rounded focus:ring-0 bg-[#efefef] checked:bg-[#dddddd] appearance-none transition-all"
-                  name="massiva"
-                  id="massiva"
-                />
-                <label htmlFor="massiva" className="select-none">
-                  Aviso Massivas?
-                </label>
-                <svg
-                  className="absolute w-4 h-4 mt-1 opacity-0 peer-checked:opacity-100 block pointer-events-none transition-opacity duration-200 ease-in-out"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="4"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <polyline points="20 6 9 17 4 12"></polyline>
-                </svg>
+              <div className="flex justify-center w-90% py-2 border-b-2 border-[#efefef] pb-3">
+                <div className="ml-10 flex">
+                  <input
+                    value={formData.massiva}
+                    onChange={handleInputChange}
+                    type="checkbox"
+                    checked={formData.massiva}
+                    className="peer mt-1 mr-2 w-4 h-4 rounded focus:ring-0 bg-[#efefef] checked:bg-[#dddddd] appearance-none transition-all"
+                    name="massiva"
+                    id="massiva"
+                  />
+                  <label htmlFor="massiva" className="select-none">
+                    Aviso Massivas?
+                  </label>
+                  <svg
+                    className="absolute w-4 h-4 mt-1 opacity-0 peer-checked:opacity-100 block pointer-events-none transition-opacity duration-200 ease-in-out"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <polyline points="20 6 9 17 4 12"></polyline>
+                  </svg>
+                </div>
+                <div>
+                  <label
+                    className=" ml-10 inline-block pr-[0.15rem]"
+                    htmlFor="flexswitch"
+                  >
+                    MON-SC
+                  </label>
+                  <input
+                    className="ml-2 mr-2 mt-[0.3rem] h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-neutral-300 before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-neutral-100 after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-primary checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:bg-neutral-600 dark:after:bg-neutral-400 dark:checked:bg-primary dark:checked:after:bg-primary dark:focus:before:shadow-[3px_-1px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca]"
+                    type="checkbox"
+                    role="switch"
+                    id="flexswitch"
+                    onChange={handleUniChange}
+                    checked={isChecked}
+                  />
+                  <label
+                    className="inline-block pl-[0.15rem]"
+                    htmlFor="flexswitch"
+                  >
+                    MON-RS
+                  </label>
+                </div>
               </div>
               <div className="flex mt-6">
                 <button
